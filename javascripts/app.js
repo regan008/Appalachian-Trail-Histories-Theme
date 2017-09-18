ba2298764140d26db0ef6a99ba613a2852457053
Mandy Regan <Mandy@MacBook-Pro-51.local>
2017-09-18 12:34:13 -0400

    button classes

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function($){
 $(document).foundation();
 $('a[rel="tag"]').addClass("label label-primary").prepend('<i class="icon-tag icon-white"></i> ');
-  $(".popular").addClass("button round tiny").prepend('<i class="icon-tag"></i> ');
-  $('.v-popular,.vv-popular,.vvv-popular').addClass("btn btn-small btn-info").prepend('<i class="icon-tag"></i> ');
-  $('.vvvv-popular,.vvvvv-popular,.vvvvvv-popular').addClass("btn btn-primary").prepend('<i class="icon-tag"></i> ');
-  $('.vvvvvvv-popular,.vvvvvvvv-popular').addClass("btn btn-large btn-success").prepend('<i class="icon-tag"></i> ');
+  $(".popular").addClass("button tiny").prepend('<i class="icon-tag"></i> ');
+  $('.v-popular,.vv-popular,.vvv-popular').addClass("button small").prepend('<i class="icon-tag"></i> ');
+  $('.vvvv-popular,.vvvvv-popular,.vvvvvv-popular').addClass("button").prepend('<i class="icon-tag"></i> ');
+  $('.vvvvvvv-popular,.vvvvvvvv-popular').addClass("button large").prepend('<i class="icon-tag"></i> ');
 
 });
