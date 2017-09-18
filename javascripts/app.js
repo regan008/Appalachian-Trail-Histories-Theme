84de873b91fdcb827e0ece3bf4679eb9a5183b4a
Mandy Regan <Mandy@MacBook-Pro-51.local>
2017-09-18 12:51:27 -0400

    fix size of buttons

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,9 +1,8 @@
 jQuery(document).ready(function($){
 $(document).foundation();
 $('a[rel="tag"]').addClass("label label-primary").prepend('<i class="icon-tag icon-white"></i> ');
-  $(".popular").addClass("button tiny");
-  $('.v-popular,.vv-popular,.vvv-popular').addClass("button small").prepend('<i class="icon-tag"></i> ');
-  $('.vvvv-popular,.vvvvv-popular,.vvvvvv-popular').addClass("button").prepend('<i class="icon-tag"></i> ');
-  $('.vvvvvvv-popular,.vvvvvvvv-popular').addClass("button large").prepend('<i class="icon-tag"></i> ');
+  $(".popular").addClass("button small");
+  $('.v-popular,.vv-popular,.vvv-popular').addClass("button").prepend('<i class="icon-tag"></i> ');
+  $('.vvvv-popular,.vvvvv-popular,.vvvvvv-popular, .vvvvvvv-popular,.vvvvvvvv-popular').addClass("button large").prepend('<i class="icon-tag"></i> ');
 
 });
