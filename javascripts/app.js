ff806547a80b294e2b29e9b8478254953e55839b
Mandy Regan <Mandy@MacBook-Pro-51.local>
2017-09-18 12:21:03 -0400

    add js to format buttons

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,3 +1,9 @@
 jQuery(document).ready(function($){
 $(document).foundation();
+$('a[rel="tag"]').addClass("label label-primary").prepend('<i class="icon-tag icon-white"></i> ');
+$(".popular").addClass("button round tiny").prepend('<i class="icon-tag"></i> ');
+$('.v-popular,.vv-popular,.vvv-popular').addClass("button round small").prepend('<i class="icon-tag"></i> ');
+$('.vvvv-popular,.vvvvv-popular,.vvvvvv-popular').addClass("button round").prepend('<i class="icon-tag"></i> ');
+$('.vvvvvvv-popular,.vvvvvvvv-popular').addClass("button round large").prepend('<i class="icon-tag"></i> ');
+
 });
